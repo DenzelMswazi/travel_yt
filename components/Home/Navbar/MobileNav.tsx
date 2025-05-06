1b975459bec0021aacaf392177dfaff23ba1943c
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-06 16:16:45 +0300

    pass the openNav function to the Mobile navigation

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -3,14 +3,20 @@ import Link from 'next/link';
 import React from 'react'
 import { CgClose } from 'react-icons/cg';
 
-const MobileNav = () => {
+type Props = {
+  showNav: boolean;
+  closeNav: () => void;
+}
+
+const MobileNav = ({closeNav, showNav}:Props) => {
+ const navOpen = showNav ? 'translate-x-0' : 'translate-x-[-100%]';
   return (
     <div>
       {/* Overlay */}
-      <div className='fixed inset-0 transform transition-all duration-500 z-[1002] bg-black opacity-70 w-full h-screen'></div>
+      <div className={`fixed ${navOpen} inset-0 transform transition-all duration-500 z-[1002] bg-black opacity-70 w-full h-screen`}></div>
       {/* NavLinks */}
-      <div className='text-wide fixed justify-center flex flex-col h-full transform transition-all duration-500
-      delay-300 w-[80%] sm:w-[60%] bg-rose-900 space-y-6 z-[1050]'>
+      <div className={`text-white ${navOpen} fixed justify-center flex flex-col h-full transform transition-all duration-500
+      delay-300 w-[80%] sm:w-[60%] bg-rose-900 space-y-6 z-[1050]`}>
         {navLinks.map((link) => {
           return <Link key={link.id} href={link.url}>
             <p className='text-white w-fit text-[20px] ml-12 border-b-[1.5px] pb-1 border-white sm:text-[30px]'>
@@ -19,7 +25,9 @@ const MobileNav = () => {
           </Link>
         })}
          {/* Close Button */}
-      <CgClose className='absolute top-[0.7rem] right-[1.4rem] sm:w-8 sm:h-8 w-6 h-6 text-white'/>
+      <CgClose 
+      onClick={closeNav}
+      className='absolute top-[0.7rem] right-[1.4rem] sm:w-8 sm:h-8 w-6 h-6 text-white'/>
       </div>
     </div>
   )
