34e361b8c2f6382e903fa9713b316e2221eeee20
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-06 19:52:11 +0300

    fix the navbar functionality

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -1,36 +1,46 @@
-import { navLinks } from '@/constant/constant';
-import Link from 'next/link';
-import React from 'react'
-import { CgClose } from 'react-icons/cg';
+import { navLinks } from "@/constant/constant";
+import Link from "next/link";
+import React from "react";
+import { CgClose } from "react-icons/cg";
 
 type Props = {
   showNav: boolean;
   closeNav: () => void;
-}
+};
 
-const MobileNav = ({closeNav, showNav}:Props) => {
- const navOpen = showNav ? 'translate-x-0' : 'translate-x-[-100%]';
+const MobileNav = ({ closeNav, showNav }: Props) => {
+  const navOpen = showNav ? "translate-x-0" : "-translate-x-full";
+
   return (
-    <div>
+    <>
       {/* Overlay */}
-      <div className={`fixed ${navOpen} inset-0 transform transition-all duration-500 z-[1002] bg-black opacity-70 w-full h-screen`}></div>
-      {/* NavLinks */}
-      <div className={`text-white ${navOpen} fixed justify-center flex flex-col h-full transform transition-all duration-500
-      delay-300 w-[80%] sm:w-[60%] bg-rose-900 space-y-6 z-[1050]`}>
-        {navLinks.map((link) => {
-          return <Link key={link.id} href={link.url}>
-            <p className='text-white w-fit text-[20px] ml-12 border-b-[1.5px] pb-1 border-white sm:text-[30px]'>
+      {showNav && (
+        <div
+          onClick={closeNav}
+          className="fixed inset-0 bg-black opacity-70 z-[1001]"
+        />
+      )}
+
+      {/* Mobile Menu */}
+      <div
+        className={`fixed top-0 left-0 h-full w-[80%] sm:w-[60%] bg-rose-900 text-white z-[1050] transform transition-transform duration-500 ${navOpen} flex flex-col justify-center space-y-6`}
+      >
+        {navLinks.map((link) => (
+          <Link key={link.id} href={link.url}>
+            <p className="text-white w-fit text-[20px] ml-12 border-b-[1.5px] pb-1 border-white sm:text-[30px]">
               {link.label}
             </p>
           </Link>
-        })}
-         {/* Close Button */}
-      <CgClose 
-      onClick={closeNav}
-      className='absolute top-[0.7rem] right-[1.4rem] sm:w-8 sm:h-8 w-6 h-6 text-white'/>
+        ))}
+
+        {/* Close Button */}
+        <CgClose
+          onClick={closeNav}
+          className="absolute top-4 right-5 sm:w-8 sm:h-8 w-6 h-6 text-white cursor-pointer"
+        />
       </div>
-    </div>
-  )
-}
+    </>
+  );
+};
 
 export default MobileNav;
