fd6f21bc6b4059d2acbaa972e34b48a4e5b2a54a
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-06 15:45:41 +0300

    add function to show or hide navigation

--- components/Home/Navbar/ResponsiveNav.tsx
+++ components/Home/Navbar/ResponsiveNav.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+"use client";
+
+import React, { useState} from 'react'
 import Nav from './Nav'
 import MobileNav from './MobileNav'
 
 const ResponsiveNav = () => {
+  const [showNav, setShowNav] = useState(false);
+  const handleNavShow = () => setShowNav(true);
+  const handleCloseNav = () => setShowNav(false);
+  
   return (
     <div>
         <Nav />
