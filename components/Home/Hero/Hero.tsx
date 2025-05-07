8f2c994e49ef9b4a7ff7a329dcdd38129515d3f2
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-07 13:12:05 +0300

    add a bg color to the hero section

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='relative w-full h-[120vh] sm:h-[100vh]'>
+        <div className='absolute top-0 left-0 w-full h-full bg-gray-800 opacity-70'></div>
+    </div>
   )
 }
 
