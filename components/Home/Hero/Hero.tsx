64fbb259204882b44019d1d6f0f628144f6b85a1
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-07 14:00:42 +0300

    add hero section video

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Hero = () => {
   return (
     <div className='relative w-full h-[120vh] sm:h-[100vh]'>
+        {/* Video Overlay */}
         <div className='absolute top-0 left-0 w-full h-full bg-gray-800 opacity-70'></div>
+        <video src="/images/hero1.mp4" autoPlay muted loop preload="metadata" className='w-full h-full object-cover'/>
     </div>
   )
 }
