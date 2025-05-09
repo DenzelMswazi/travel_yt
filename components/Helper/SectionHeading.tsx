e681d7b665555d13e9e8065b4dca0338e17fb7ef
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-10 01:00:44 +0300

    add classnames to the descriptive text

--- components/Helper/SectionHeading.tsx
+++ components/Helper/SectionHeading.tsx
@@ -7,8 +7,8 @@ type Props = {
 const SectionHeading = ({heading}:Props) => {
   return (
     <div className='w-[80] mx-auto'>
-      <h1>{heading}</h1>
-      <p>At Trippie, Every destination has a story to tell. From the sun-kissed beaches of Bali to the snow-covered peaks of Swiss Alps.
+      <h1 className='text-xl sm:text-3xl text-blue-950 font-bold'>{heading}</h1>
+      <p className='mt-2 text-gray-700 sm:text-base text-sm font-medium'>At Trippie, Every destination has a story to tell. From the sun-kissed beaches of Bali to the snow-covered peaks of Swiss Alps.
         We bring the world closer to you - One unforgettable experience at a time.</p>
     </div>
   )
