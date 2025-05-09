be7e3f634254efa4289cb0aa6bdcff85139d20e3
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-10 01:02:33 +0300

    add heading to the destinations sections

--- components/Home/Destination/Destination.tsx
+++ components/Home/Destination/Destination.tsx
@@ -1,8 +1,11 @@
+import SectionHeading from '@/components/Helper/SectionHeading';
 import React from 'react'
 
 const Destination = () => {
   return (
-    <div className='pt-20 pb-20'>Destination</div>
+    <div className='pt-20 pb-20'>
+      <SectionHeading heading='Explore Our Popular Destinations' />
+    </div>
   )
 }
 
