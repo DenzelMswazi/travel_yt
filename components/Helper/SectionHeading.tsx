4e9873ee0628b8262b460d897d2996cee67104e6
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-10 01:27:24 +0300

    add styling to the section content

--- components/Helper/SectionHeading.tsx
+++ components/Helper/SectionHeading.tsx
@@ -4,7 +4,7 @@ type Props = {
     heading: string;
 }
 
-const SectionHeading = ({heading}:Props) => {
+const SectionHeading = ({heading}: Props) => {
   return (
     <div className='w-[80] mx-auto'>
       <h1 className='text-xl sm:text-3xl text-blue-950 font-bold'>{heading}</h1>
--- components/Home/Destination/Destination.tsx
+++ components/Home/Destination/Destination.tsx
@@ -5,6 +5,9 @@ const Destination = () => {
   return (
     <div className='pt-20 pb-20'>
       <SectionHeading heading='Explore Our Popular Destinations' />
+      <div className='mt-14 w-[80%] mx-auto'>
+
+      </div>
     </div>
   )
 }
