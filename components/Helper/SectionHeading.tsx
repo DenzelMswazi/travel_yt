3a2dffe079148ab0d2c004a1dc0a02f9ffe58c5c
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-10 00:57:40 +0300

    add a descritive text to the features section

--- components/Helper/SectionHeading.tsx
+++ components/Helper/SectionHeading.tsx
@@ -6,7 +6,11 @@ type Props = {
 
 const SectionHeading = ({heading}:Props) => {
   return (
-    <div>SectionHeading</div>
+    <div className='w-[80] mx-auto'>
+      <h1>{heading}</h1>
+      <p>At Trippie, Every destination has a story to tell. From the sun-kissed beaches of Bali to the snow-covered peaks of Swiss Alps.
+        We bring the world closer to you - One unforgettable experience at a time.</p>
+    </div>
   )
 }
 
