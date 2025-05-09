f7855638105f9574b2dfc1f35a676070a574b120
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-09 18:49:01 +0300

    add the section heading Props

---
+++ components/Helper/SectionHeading.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {
+    heading: string;
+}
+
+const SectionHeading = ({}:Props) => {
+  return (
+    <div>SectionHeading</div>
+  )
+}
+
+export default SectionHeading;
