1aae3584445036288c95b27a9473c06564e86c74
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-10 10:55:47 +0300

    update the styling for the destination text

--- components/Helper/SectionHeading.tsx
+++ components/Helper/SectionHeading.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 
 type Props = {
-    heading: string;
+  heading: string;
 }
 
-const SectionHeading = ({heading}: Props) => {
+const SectionHeading = ({ heading }: Props) => {
   return (
-    <div className='w-[80] mx-auto'>
-      <h1 className='text-xl sm:text-3xl text-blue-950 font-bold'>{heading}</h1>
-      <p className='mt-2 text-gray-700 sm:text-base text-sm font-medium'>At Trippie, Every destination has a story to tell. From the sun-kissed beaches of Bali to the snow-covered peaks of Swiss Alps.
-        We bring the world closer to you - One unforgettable experience at a time.</p>
+    <div className="px-4 sm:px-8 max-w-3xl">
+      <h1 className="text-xl sm:text-3xl text-blue-950 font-bold text-left">{heading}</h1>
+      <p className="mt-2 text-gray-700 sm:text-base text-sm font-medium text-left">
+        At Trippie, every destination has a story to tell. From the sun-kissed beaches of Bali to the snow-covered peaks of the Swiss Alps.
+        We bring the world closer to you — one unforgettable experience at a time.
+      </p>
     </div>
   )
 }
