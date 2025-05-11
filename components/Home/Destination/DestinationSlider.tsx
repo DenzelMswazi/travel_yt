9fb63aeafcabe1ae42d8c8b6df02a31ce0591dcf
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-11 11:19:03 +0300

    add a multi carousel package from react

--- components/Home/Destination/DestinationSlider.tsx
+++ components/Home/Destination/DestinationSlider.tsx
@@ -1,5 +1,26 @@
 import React from 'react'
 
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1324 },
+    items: 5,
+    slidesToSlide: 1 // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1324, min: 764 },
+    items: 2,
+    slidesToSlide: 1 // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 764, min: 0 },
+    items: 1,
+    slidesToSlide: 1 // optional, default to 1.
+  }
+};
+
 const DestinationSlider = () => {
   return (
     <div>DestinationSlider</div>
