6b7eca9b8c1a59bb3b91fc39f24e51d7bfde6b75
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-11 11:55:44 +0300

    add destination content

--- components/Home/Destination/DestinationSlider.tsx
+++ components/Home/Destination/DestinationSlider.tsx
@@ -35,6 +35,9 @@ const DestinationSlider = () => {
             <div className='absolute inset-0 bg-black opacity-25 rounded-lg'></div>
             {/* Image */}
             <Image src={data.image} alt={data.country} width={500} height={500} className="h-full w-full object-cover rounded-lg"/>
+            {/* Content */}
+            <h1>{data.country}</h1>
+            <p>{data.travelers}</p>
           </div>
         </div>
       })}
