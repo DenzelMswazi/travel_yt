061f16803ec078b7a37d89c3c2acbc1cf09f651d
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-12 18:50:02 +0300

    imprt section headings

--- components/Home/Hotel/Hotel.tsx
+++ components/Home/Hotel/Hotel.tsx
@@ -1,10 +1,12 @@
+import SectionHeading from '@/components/Helper/SectionHeading';
 import React from 'react'
 
 const Hotel = () => {
   return (
     <div className='pt-20 pb-20'>
         {/* Section Heading */}
-        
+    <SectionHeading heading='Recommended Hotels'/>  
+    <div></div>
     </div>
   )
 }
