d9e923902214be241666d8c3da6e4c90754e93c3
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-12 22:39:11 +0300

    add hotel rating

--- components/Home/Hotel/HotelCard.tsx
+++ components/Home/Hotel/HotelCard.tsx
@@ -30,7 +30,12 @@ const HotelCard = ({hotel}: Props) => {
     <div>
         <h1 className='mt-4 text-lg font-semibold text-blue-950 hover:text-black cursor-pointer transition-all duration-200'>{hotel.name}</h1>
         <p className='text-sm text-gray-600 mt-3 font-medium mb-6'>{hotel.location}</p>
-
+       {/* Rating */}
+        <div className='flex items-center space-x-2'>
+            <div className='px-2 py-2 bg-blue-800 rounded-md font-bold text-white tex-xs'>
+                {hotel.rating}
+            </div>
+        </div>
     </div>
     </div>
   )
