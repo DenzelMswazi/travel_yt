eb9c7631d4869b9f1f1302ed26541a0c6869192c
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-12 21:44:53 +0300

    add an overlay to the hotel card

--- components/Home/Hotel/HotelCard.tsx
+++ components/Home/Hotel/HotelCard.tsx
@@ -20,6 +20,8 @@ const HotelCard = ({hotel}: Props) => {
         <div className='absolute top-4 right-4 z-20 w-8 h-8 bg-white rounded-full text-black flex items-center justify-center flex-col'>
             <FaHeart className='w-3 h-3'/>
         </div>
+        {/* Overlay */}
+        <div className='absolute inset-0 bg-black opacity-20 z-20'></div>
     </div>
     </div>
   )
