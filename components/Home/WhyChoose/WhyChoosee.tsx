59311edf826fe9a34ec5547e7fdce75e91919b91
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-14 12:01:18 +0300

    add section heading to the component

--- components/Home/WhyChoose/WhyChoosee.tsx
+++ components/Home/WhyChoose/WhyChoosee.tsx
@@ -1,9 +1,13 @@
+import SectionHeading from '@/components/Helper/SectionHeading';
 import React from 'react'
 
 const WhyChoosee = () => {
   return (
-    <div>WhyChoosee</div>
+    <div className='pt-16 pb-24'>
+        {/* Section Title */}
+        <SectionHeading heading='Why Choose Us?' />
+    </div>
   )
 }
 
-export default WhyChoosee
+export default WhyChoosee;  
