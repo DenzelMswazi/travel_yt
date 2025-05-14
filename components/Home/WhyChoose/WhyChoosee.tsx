3eec829b610896c2cba8585eb5df748fc4c1acaa
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-14 17:03:37 +0300

    update reviews import

--- components/Home/WhyChoose/WhyChoosee.tsx
+++ components/Home/WhyChoose/WhyChoosee.tsx
@@ -1,5 +1,6 @@
 import SectionHeading from '@/components/Helper/SectionHeading';
 import React from 'react'
+import WhyChooseCard from './WhyChooseCard';
 
 const WhyChoosee = () => {
   return (
@@ -7,7 +8,9 @@ const WhyChoosee = () => {
         {/* Section Title */}
         <SectionHeading heading='Why Choose Us?' />
         <div className='grid w-[80%] mx-auto grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 items-center mt-20'>
-            
+        <div>
+            <WhyChooseCard />
+            </div>  
         </div>
     </div>
   )
