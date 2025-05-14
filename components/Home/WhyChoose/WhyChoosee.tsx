62c9b1a92e0632e535b16c11c6182156830e5bc3
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-14 21:19:41 +0300

    add text titles to the reviews page

--- components/Home/WhyChoose/WhyChoosee.tsx
+++ components/Home/WhyChoose/WhyChoosee.tsx
@@ -9,13 +9,13 @@ const WhyChoosee = () => {
         <SectionHeading heading='Why Choose Us?' />
         <div className='grid w-[80%] mx-auto grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 items-center mt-20'>
         <div>
-            <WhyChooseCard image="/images/c1.svg" title="Best Price Guarantee"/>
+            <WhyChooseCard image="/images/c1(1).svg" title="Best Price Guarantee"/>
             </div>  
             <div>
-            <WhyChooseCard image="/images/c2.svg" title="Best Price Guarantee"/>
+            <WhyChooseCard image="/images/c2.svg" title="Easy & Quick Booking"/>
             </div>
             <div>
-            <WhyChooseCard image="/images/c3.svg" title="Best Price Guarantee"/>
+            <WhyChooseCard image="/images/c3(1).svg" title="Customer Care"/>
             </div>
         </div>
     </div>
