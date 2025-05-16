def7739f0d319a8e424ffac9c49fae8752216057
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-16 11:42:08 +0300

    initial commit for description text

--- components/Home/WhyChoose/WhyChooseCard.tsx
+++ components/Home/WhyChoose/WhyChooseCard.tsx
@@ -10,7 +10,7 @@ const WhyChooseCard = ({ image, title}: Props) => {
   return (
     <div>
       <Image src={image} width={70} height={70} alt='image' className='mx-auto'/>
-      <h1>{title}</h1>
+      <h1 className='mt-6 text-center text-gray-900 font-medium text-lg'>{title}</h1>
       <p>Description text will be added here, For more information check contact details</p>
     </div>
   )
