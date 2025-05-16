cad4998ccba9915f8e22b9c4d27e4876b1ae8aee
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-16 11:44:20 +0300

    final styling for the description text

--- components/Home/WhyChoose/WhyChooseCard.tsx
+++ components/Home/WhyChoose/WhyChooseCard.tsx
@@ -11,7 +11,7 @@ const WhyChooseCard = ({ image, title}: Props) => {
     <div>
       <Image src={image} width={70} height={70} alt='image' className='mx-auto'/>
       <h1 className='mt-6 text-center text-gray-900 font-medium text-lg'>{title}</h1>
-      <p>Description text will be added here, For more information check contact details</p>
+      <p className='mt-2 text-center text-xs font-medium text-gray-700'>Description text will be added here, For more information check contact details</p>
     </div>
   )
 }
