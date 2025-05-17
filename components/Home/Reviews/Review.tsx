32bba4546408de46d83662fa219be02cd84aa1b5
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-17 07:49:49 +0300

    add the blue background for the reviews page

--- components/Home/Reviews/Review.tsx
+++ components/Home/Reviews/Review.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Review = () => {
   return (
-    <div>Review</div>
+    <div className='pt-20 pb-20 flex items-center justify-center flex-col bg-[#13357b]'>Review</div>
   )
 }
 
