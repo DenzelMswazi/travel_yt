49d5b3cb8635f86fa8ecdd5c33bbb137c1944e0d
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-17 07:46:57 +0300

    import reviews component to home page

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -3,6 +3,7 @@ import Hero from './Hero/Hero';
 import Destination from './Destination/Destination';
 import Hotel from './Hotel/Hotel';
 import WhyChoosee from './WhyChoose/WhyChoosee';
+import Review from './Reviews/Review';
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Destination />
       <Hotel />
       <WhyChoosee />
+      <Review />
     </div>
   )
 }
