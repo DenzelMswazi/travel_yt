243736a03ea99e4b030ee8e38a44872ca145e6ba
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-26 10:07:34 +0300

    chore: heading styling

--- components/Home/Reviews/Review.tsx
+++ components/Home/Reviews/Review.tsx
@@ -5,7 +5,7 @@ const Review = () => {
     <div className='pt-20 pb-20 flex items-center justify-center flex-col bg-[#13357b]'>
       <div className='w-[80%] mx-auto grid items-center grid-cols-1 lg:grid-cols-2 gap-10'>
         <div>
-          <h1>What Our Customers are Saying About Us?</h1>
+          <h1 className='text-2xl font-semibold text-white'>What Our Customers are Saying About Us?</h1>
         </div>
       </div>
     </div>
