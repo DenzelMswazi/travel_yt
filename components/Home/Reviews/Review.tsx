395d9732d7c56c766401b9229521975b1354920d
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-26 10:16:37 +0300

    chore: add descpription text

--- components/Home/Reviews/Review.tsx
+++ components/Home/Reviews/Review.tsx
@@ -5,7 +5,10 @@ const Review = () => {
     <div className='pt-20 pb-20 flex items-center justify-center flex-col bg-[#13357b]'>
       <div className='w-[80%] mx-auto grid items-center grid-cols-1 lg:grid-cols-2 gap-10'>
         <div>
-          <h1 className='text-2xl font-semibold text-white'>What Our Customers are Saying About Us?</h1>
+          <h1 className='text-2xl font-semibold text-white'>Don't take Our Word for it - Here's what our customers think?</h1>
+          <p>We believe the best way to understand the impact of our product is to hear directly from the people
+            who use it everyday. Our custmers inspire us with their stories, successes and honest feedback. Here's what they have to say about their experience with Trippie.
+          </p>
         </div>
       </div>
     </div>
