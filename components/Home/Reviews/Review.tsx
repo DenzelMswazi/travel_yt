7d0c5a1ae1e72dd49a5aadd0c56864393ebc94d5
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-26 10:18:30 +0300

    feat: style the descriptive text

--- components/Home/Reviews/Review.tsx
+++ components/Home/Reviews/Review.tsx
@@ -6,7 +6,7 @@ const Review = () => {
       <div className='w-[80%] mx-auto grid items-center grid-cols-1 lg:grid-cols-2 gap-10'>
         <div>
           <h1 className='text-2xl font-semibold text-white'>Don't take Our Word for it - Here's what our customers think?</h1>
-          <p>We believe the best way to understand the impact of our product is to hear directly from the people
+          <p className='mt-6 text-gray-200'>We believe the best way to understand the impact of our product is to hear directly from the people
             who use it everyday. Our custmers inspire us with their stories, successes and honest feedback. Here's what they have to say about their experience with Trippie.
           </p>
         </div>
