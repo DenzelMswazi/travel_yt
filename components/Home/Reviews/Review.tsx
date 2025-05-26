6b792baedb51c45f0522c83ea9934c28a579cdf4
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-26 10:06:09 +0300

    add reviews section heading

--- components/Home/Reviews/Review.tsx
+++ components/Home/Reviews/Review.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Review = () => {
   return (
     <div className='pt-20 pb-20 flex items-center justify-center flex-col bg-[#13357b]'>
-      <div className='w-[80%] mx-auto grid items-center grid-cols-1 lg:grid-cols-2 gap-10'></div>
+      <div className='w-[80%] mx-auto grid items-center grid-cols-1 lg:grid-cols-2 gap-10'>
+        <div>
+          <h1>What Our Customers are Saying About Us?</h1>
+        </div>
+      </div>
     </div>
   )
 }
