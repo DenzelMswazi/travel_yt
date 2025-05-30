4f6e51e113b638358eeae2c41b2e0fa043cd8384
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-30 23:05:13 +0300

    chore: add the swipper cards to the reviews page

--- components/Home/Reviews/ReviewSlider.tsx
+++ components/Home/Reviews/ReviewSlider.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
+'use client'
+
+import React from 'react';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/effect-cards';
-
 import { EffectCards } from 'swiper/modules';
 
 const ReviewSlider = () => {
   return (
-    <div>ReviewSlider</div>
+    <div>
+      <Swiper 
+      effect={'cards'} 
+      grabCursor={true} 
+      modules={[EffectCards]} 
+      className="md:w-[450px] md:h-[350px] w-[90px] h-[300px]">
+
+      </Swiper>
+      </div>
   )
 }
 
