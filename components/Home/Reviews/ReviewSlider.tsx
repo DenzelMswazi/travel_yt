fc288ea0396c3bf5cefd028aeeb4abaca38eae6b
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-30 22:52:57 +0300

    chore: add swipper modules

--- components/Home/Reviews/ReviewSlider.tsx
+++ components/Home/Reviews/ReviewSlider.tsx
@@ -4,6 +4,8 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/effect-cards';
 
+import {} from 'swiper/modules';
+
 const ReviewSlider = () => {
   return (
     <div>ReviewSlider</div>
