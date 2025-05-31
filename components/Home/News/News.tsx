5b2e273253be6e6cfc50b517a870cb5b7d551d73
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-31 13:51:36 +0300

    chore: add news card import

--- components/Home/News/News.tsx
+++ components/Home/News/News.tsx
@@ -1,11 +1,16 @@
 import SectionHeading from '@/components/Helper/SectionHeading';
 import React from 'react'
+import NewsCard from './NewsCard';
 
 const News = () => {
   return (
     <div className='pt-16 pb-16'>
      <SectionHeading heading="Travel News for You" />   
-     <div className='w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 items-center mt-20'></div>
+     <div className='w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 items-center mt-20'>
+        <div>
+            <NewsCard />
+        </div>
+     </div>
     </div>
   )
 }
