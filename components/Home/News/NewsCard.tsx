4762343531622686b783cfc9eb9d01f5cc3b42ec
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-31 14:33:28 +0300

    chore: map the destination image

--- components/Home/News/NewsCard.tsx
+++ components/Home/News/NewsCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { title } from 'process'
 import React from 'react'
 
@@ -9,7 +10,11 @@ type Props = {
 
 const NewsCard = ({ date, image, title }:Props ) => {
   return (
-    <div>NewsCard</div>
+    <div>
+      <div className='h-[300px]'>
+        <Image src={image} alt={title} width={300} height={300} className='w-full h-full object-cover rounded-lg'/>
+        </div>  
+    </div>
   )
 }
 
