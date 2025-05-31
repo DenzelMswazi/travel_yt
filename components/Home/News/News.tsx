e7f9d17ed4bef0416610c0e94c35866ab592e11a
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-31 13:28:52 +0300

    chore: add section heading import

--- components/Home/News/News.tsx
+++ components/Home/News/News.tsx
@@ -1,9 +1,10 @@
+import SectionHeading from '@/components/Helper/SectionHeading';
 import React from 'react'
 
 const News = () => {
   return (
     <div className='pt-16 pb-16'>
-        
+     <SectionHeading />   
     </div>
   )
 }
