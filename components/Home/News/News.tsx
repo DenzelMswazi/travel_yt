b408c99fe51473874642f6bd420e9df2a597f300
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-31 13:37:00 +0300

    chore: add a grid column for the news

--- components/Home/News/News.tsx
+++ components/Home/News/News.tsx
@@ -5,6 +5,7 @@ const News = () => {
   return (
     <div className='pt-16 pb-16'>
      <SectionHeading heading="Travel News for You" />   
+     <div className='w-[80%] mx-auto grid grid-cols-1'></div>
     </div>
   )
 }
