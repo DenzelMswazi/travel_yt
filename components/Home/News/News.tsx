ee105d47ab7f6451e562ab0fca226d41a5b2df0d
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-31 14:01:41 +0300

    chore: add destination images

--- components/Home/News/News.tsx
+++ components/Home/News/News.tsx
@@ -8,7 +8,7 @@ const News = () => {
      <SectionHeading heading="Travel News for You" />   
      <div className='w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 items-center mt-20'>
         <div>
-            <NewsCard />
+            <NewsCard image="/images/n1.jpg" title="Top 10 places to visit suggested by Tripppie"/>
         </div>
      </div>
     </div>
