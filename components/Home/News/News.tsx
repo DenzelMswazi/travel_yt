bc799e4f49a610d349b5f4085ddfea9ac266c84b
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-05-31 13:44:53 +0300

    chore: create news card

--- components/Home/News/News.tsx
+++ components/Home/News/News.tsx
@@ -5,7 +5,7 @@ const News = () => {
   return (
     <div className='pt-16 pb-16'>
      <SectionHeading heading="Travel News for You" />   
-     <div className='w-[80%] mx-auto grid grid-cols-1'></div>
+     <div className='w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 items-center mt-20'></div>
     </div>
   )
 }
