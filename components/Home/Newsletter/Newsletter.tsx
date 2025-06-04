413134be5dbb8e538988f73cb7d9f16471047032
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-06-04 10:31:22 +0300

    chore: add inline styling for the icon

--- components/Home/Newsletter/Newsletter.tsx
+++ components/Home/Newsletter/Newsletter.tsx
@@ -4,7 +4,7 @@ import { BsEnvelopePaper } from 'react-icons/bs';
 const Newsletter = () => {
   return (
     <div className='bg-black pt-16 pb-16 flex items-center justify-center w-full flex-col'>
-        <BsEnvelopePaper />
+        <BsEnvelopePaper className='w-16 h-16 mt-20 text-white'/>
         </div>
   )
 }
