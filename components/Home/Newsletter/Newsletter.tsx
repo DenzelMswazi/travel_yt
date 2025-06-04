30f2a6afa7e49b66e68f9df2e68da39629873cfd
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-06-04 10:37:30 +0300

    chore: add inline styling for the placeholder heading

--- components/Home/Newsletter/Newsletter.tsx
+++ components/Home/Newsletter/Newsletter.tsx
@@ -5,7 +5,8 @@ const Newsletter = () => {
   return (
     <div className='bg-black pt-16 pb-16 flex items-center justify-center w-full flex-col'>
         <BsEnvelopePaper className='w-16 h-16 mt-20 text-white'/>
-        <h1>Your Travel Journey Starts Here</h1>
+        <h1 className='text-lg sm:text-xl md:text-2xl text-white font-semibold mt-10 tracking-widest'>Your Travel Journey Starts Here</h1>
+        <p>Sign Up for the best Travelling deals on your favorite destination</p>
         </div>
   )
 }
