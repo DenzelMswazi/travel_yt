be2de4125eacfb20695f14e62a786d5883976045
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-06-04 10:45:57 +0300

    chore: add email input area

--- components/Home/Newsletter/Newsletter.tsx
+++ components/Home/Newsletter/Newsletter.tsx
@@ -6,7 +6,13 @@ const Newsletter = () => {
     <div className='bg-black pt-16 pb-16 flex items-center justify-center w-full flex-col'>
         <BsEnvelopePaper className='w-16 h-16 mt-20 text-white'/>
         <h1 className='text-lg sm:text-xl md:text-2xl text-white font-semibold mt-10 tracking-widest'>Your Travel Journey Starts Here</h1>
-        <p>Sign Up for the best Travelling deals on your favorite destination</p>
+        <p className='mt-3 text-white text-xs md:text-sm'>Sign Up for the best Travelling deals on your favorite destination</p>
+        <div className='w-full'>
+            <input type='text' 
+            className='px-6 py-3.5 bg-white mt-8 w-[95%] sm:w-[80%] md:w-[60%] lg:w-[40%] block mx-auto rounded-lg outline-none'
+            placeholder='Enter Your Email'
+            />
+        </div>
         </div>
   )
 }
