4972f6dd0b054adbc051ac37010f2b65d1a223da
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-06-04 10:32:47 +0300

    chore: add placeholder heading

--- components/Home/Newsletter/Newsletter.tsx
+++ components/Home/Newsletter/Newsletter.tsx
@@ -5,6 +5,7 @@ const Newsletter = () => {
   return (
     <div className='bg-black pt-16 pb-16 flex items-center justify-center w-full flex-col'>
         <BsEnvelopePaper className='w-16 h-16 mt-20 text-white'/>
+        <h1>Your Travel Journey Starts Here</h1>
         </div>
   )
 }
