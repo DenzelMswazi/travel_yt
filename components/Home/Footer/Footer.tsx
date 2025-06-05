2da8394727456e90ab4740964ff07d93c996f6b9
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-06-05 11:15:22 +0300

    chore: add the first part of the footer

--- components/Home/Footer/Footer.tsx
+++ components/Home/Footer/Footer.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='pt-16 pb-16'>
+        <div className='w-[80%] mx-auto items-start grid-cols-1 sm:grid-cols-2 grid md:grid-cols-3 lg:grid-cols-4 gap-10'>
+            <div className='space-y-5'>
+                <h1 className='text-lg font-bold'>Company</h1>
+            </div>
+        </div>
+    </div>
   )
 }
 
